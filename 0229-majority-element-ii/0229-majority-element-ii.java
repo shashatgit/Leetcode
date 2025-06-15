ecd8779c9e5ed3ce621ed27b43d92b2f694ec2ad
shashatgit <137678630+shashatgit@users.noreply.github.com>
2025-06-15 08:31:32 +0530

    Time: 13 ms (37.02%), Space: 49.4 MB (67.19%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap <Integer,Integer> freq = new HashMap<>();
+        ArrayList <Integer> result = new ArrayList<>();
+        for (int i : nums)
+        {
+            if (freq.containsKey(i))
+            {
+                int occ = freq.get(i);
+                freq.put(i,occ+1);
+            }
+            else freq.put(i,1);
+        }
+        for (Map.Entry <Integer,Integer> e : freq.entrySet())
+        {
+            if(e.getValue() > nums.length/3)
+            {
+                result.add(e.getKey());
+            }
+        }
+        return result;
+    }
+}
