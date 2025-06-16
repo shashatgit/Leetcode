b646fb731c1d6a66cf4ce91615dea4b6c0d21860
shashatgit <137678630+shashatgit@users.noreply.github.com>
2025-06-16 22:25:34 +0530

    Time: 149 ms (5.03%), Space: 49.9 MB (100%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    let arr = []
+
+    for (let i = 0; i < nums.length; i++)
+    {
+        for (let j = 0; j<nums.length; j++)
+        {
+            if (nums[i]+nums[j] == target && i!=j)
+            {
+                arr.push(i)
+                arr.push(j)
+                return arr
+            
+            }
+        
+        
+        }
+
+    }
+   
+    
+};
