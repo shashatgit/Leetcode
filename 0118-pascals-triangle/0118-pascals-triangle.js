8e456c57c2a2270b4b0f2ebda469da41b86fa80b
shashatgit <137678630+shashatgit@users.noreply.github.com>
2025-08-02 04:38:06 +0530

    Time: 42 ms (5.02%), Space: 49.4 MB (100%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    if (numRows === 0) 
+    {
+        return [];
+    }
+    if (numRows === 1) 
+    {
+        return [[1]];
+    }
+
+    let prevRow = generate(numRows-1)
+    let newRow = new Array(numRows).fill(1)
+
+    for (let i = 1; i < numRows - 1; i++) {
+        newRow[i] = prevRow[numRows - 2][i - 1] + prevRow[numRows - 2][i];
+    }
+    
+    prevRow.push(newRow);
+    return prevRow;
+};
+
+
+    
