820d4b787b00d420cf5c4945a481c859a617bc4b
shashatgit <137678630+shashatgit@users.noreply.github.com>
2025-09-03 02:18:53 +0530

    Time: 61 ms (36.34%), Space: 18.2 MB (21.78%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -2,10 +2,8 @@ class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         result = []
         for i in range (n):
-            j = n + i
             result.append(nums[i])
-            result.append(nums[j])
+            result.append(nums[n + i])
         return result
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 
             
