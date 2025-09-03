e77c683269311fc7cec097efdd414fc7679058cb
shashatgit <137678630+shashatgit@users.noreply.github.com>
2025-09-04 00:06:26 +0530

    Time: 2 ms (58.55%), Space: 45.1 MB (42.55%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] transformArray(int[] nums) {
+        for(int i = 0; i < nums.length; i++)
+        {
+            nums[i] = nums[i] % 2;
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
