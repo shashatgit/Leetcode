67a7be85ebaeab3e9a187f233cde4a0781092cda
shashatgit <137678630+shashatgit@users.noreply.github.com>
2025-10-31 00:08:06 +0530

    Time: 3 ms (100%), Space: 56.9 MB (50.7%) - LeetHub

---
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int count = target[0];
+        for (int i = 1; i < target.length; i++)
+            count += Math.max(target[i] - target[i - 1], 0);
+        return count;
+    }
+}
