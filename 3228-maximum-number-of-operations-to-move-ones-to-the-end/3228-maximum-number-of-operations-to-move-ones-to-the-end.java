7c3371c31882e8486f3791f164732e6e70847acd
shashatgit <137678630+shashatgit@users.noreply.github.com>
2025-11-14 01:32:46 +0530

    Time: 7 ms (77.94%), Space: 47.5 MB (19.12%) - LeetHub

---
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxOperations(String s) {
+        int cnt = 0;
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            if (s.charAt(i) == '1')
+            {
+                cnt++;
+            }
+            else if(i > 0 && s.charAt(i - 1) == '1')
+            {
+                ans += cnt;
+            }
+        }
+        return ans;
+    }
+}
