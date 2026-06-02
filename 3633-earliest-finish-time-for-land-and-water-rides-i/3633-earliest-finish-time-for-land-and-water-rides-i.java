da98db0e1ae4755e2f734c2524c23ead93cd4012
Shashwat <137678630+shashatgit@users.noreply.github.com>
2026-06-02 14:17:38 +0530

    Time: 1 ms (100%), Space: 46.8 MB (61.51%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -3,25 +3,15 @@ class Solution {
         int landEndBest = 99999;
         int waterEndBest = 99999;
         int res = 99999;
-        int indL = 0; 
-        int indW = 0;
         int n = landStartTime.length;
         int m = waterStartTime.length;
         for(int i = 0; i < n; i++)
         {
-            if (landStartTime[i] + landDuration[i] < landEndBest)
-            {
-                landEndBest = landStartTime[i] + landDuration[i];
-                indL = i;
-            }
+            landEndBest = Math.min(landEndBest, landStartTime[i] + landDuration[i]);   
         }
         for(int i = 0; i < m; i++)
         {
-            if (waterStartTime[i] + waterDuration[i] < waterEndBest)
-            {
-                waterEndBest = waterStartTime[i] + waterDuration[i];
-                indW = i;
-            }
+            waterEndBest = Math.min(waterEndBest, waterStartTime[i] + waterDuration[i]);
             res = Math.min(res, Math.max(landEndBest, waterStartTime[i])+ waterDuration[i]);
         }
         for (int i = 0; i < n; i++)
@@ -29,23 +19,5 @@ class Solution {
             res = Math.min(res, Math.max(waterEndBest, landStartTime[i])+ landDuration[i]);
         }
         return res;
-
-
-        /*if(landEndBest < waterEndBest && waterStartTime[indW] <= landEndBest)
-        {
-            return landEndBest + waterDuration[indW];
-        }
-        else if (landEndBest < waterEndBest)
-        {
-            return waterEndBest;
-        }
-        else if(landEndBest > waterEndBest && waterEndBest >= landStartTime[indL])
-        {
-            return waterEndBest + landDuration[indL];
-        }
-        else
-        {
-            return landEndBest;
-        }*/
     }
 }
