8ce6c5706a16262d8eee7e77861eed838d786876
Shashwat <137678630+shashatgit@users.noreply.github.com>
2026-07-10 15:30:16 +0530

    Time: 24 ms (48.37%), Space: 47.1 MB (14.97%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
@@ -1,14 +1,18 @@
 class Solution {
-	public int similarPairs(String[] words) {
-		int ans = 0; 
-		HashMap<Integer, Integer> freq = new HashMap(); 
-		for (var word : words) {
-			int mask = 0; 
-			for (var ch : word.toCharArray()) 
-				mask |= 1<<ch-'a'; 
-			ans += freq.getOrDefault(mask, 0); 
-			freq.merge(mask, 1, Integer::sum); 
-		}
-		return ans; 
-	}
+    public int similarPairs(String[] words) {
+        Map<Set<Character>,Integer> map = new HashMap<>();
+        for(String s : words)
+        {
+            Set<Character> hs = new HashSet<>();
+            for(char ch : s.toCharArray())
+                hs.add(ch);
+            map.put(hs,map.getOrDefault(hs,0)+1);
+        }
+        int res = 0;
+        for(int count : map.values())
+        {
+            res += (count*(count-1))/2;
+        }
+        return res;
+    }
 }
