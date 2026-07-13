954b3033631247db14a9912cfe651c2722364e71
Shashwat <137678630+shashatgit@users.noreply.github.com>
2026-07-13 18:30:01 +0530

    Time: 0 ms (100%), Space: 42.5 MB (36.36%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,50 +1,15 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
-        int digits_lower = 0;
-        int low_start = 0;
-        String base = "0123456789";
-        ArrayList<Integer> ans = new ArrayList<>();
-        int og_low = low;
-
-        while (low > 0) 
-        {
-            low_start = low % 10;
-            low /= 10;
-            digits_lower++;
-        }
-
-        while (low_start > 10 - digits_lower) 
-        {
-            digits_lower++;
-            low_start = 1;
-        }
-
-        for (int i = low_start; i < 11 - digits_lower; i++) 
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int j = i; j < i + digits_lower; j++) 
-            {
-                sb.append(base.charAt(j));
-            }
-
-            int curr = Integer.parseInt(sb.toString());
-
-            if (curr >= og_low) 
-            {
-                if (curr <= high) ans.add(curr);
-                else break;
-            }
-
-            if (i == 10 - digits_lower) 
-            {
-                digits_lower++;
-
-                if (digits_lower > 9)
-                    break;
-
-                i = 0;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            int num = i;
+            for (int j = i + 1; j <= 9; j++) {
+                num = num * 10 + j;
+                if (num >= low && num <= high)
+                    res.add(num);
             }
         }
-        return ans;
+        Collections.sort(res);
+        return res;
     }
 }
