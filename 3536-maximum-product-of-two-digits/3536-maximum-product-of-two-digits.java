9d0bff47b8de49253e5e671aff7a1be598861eab
Shashwat <137678630+shashatgit@users.noreply.github.com>
2026-07-25 15:08:48 +0530

    Time: 1 ms (100%), Space: 42.7 MB (58.74%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,11 +1,22 @@
 class Solution {
     public int maxProduct(int n) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        //PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int max1 = 0;
+        int max2 = 0;
         while(n > 0)
         {
-            maxHeap.add(n % 10);
+            int digit = n % 10;
+            if(digit > max1)
+            {
+                max2 = max1;
+                max1 = digit;
+            }
+            else if(digit > max2)
+            {
+                max2 = digit;
+            }
             n = n/10;
         }
-        return maxHeap.poll()*maxHeap.poll();
+        return max1*max2;
     }
 }
