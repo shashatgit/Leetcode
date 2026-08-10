5b919a7f3142668566c313fc9b518d9bac0c7832
Shashwat <137678630+shashatgit@users.noreply.github.com>
2026-08-10 16:26:37 +0530

    Time: 7 ms (69.36%), Space: 46.4 MB (70.16%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,6 +1,8 @@
 class Solution {
     int[][] dp;
-    public int lcs(String s1, String s2, int n, int m)
+    int n;
+    int m;
+    public int lcs(String s1, String s2)
     {
         for(int i = 1; i <= n; i++)
         {
@@ -16,9 +18,9 @@ class Solution {
         return dp[n][m];
     }
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
+        n = word1.length();
+        m = word2.length();
         dp = new int[n+1][m+1];
-        return  n + m - 2*lcs(word1,word2,n,m);
+        return  n + m - 2*lcs(word1,word2);
     }
 }
